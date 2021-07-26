1631821fa05cd500561f3a0ae7ca5f7ac3b2830d
Jay Kalio <jerrykalio@gmail.com>
2021-07-25 19:20:11 -0500

    Set up checkout endpoint.

--- routes/cart.js
+++ routes/cart.js
@@ -23,83 +23,79 @@ module.exports = (app, passport) => {
    });
 
 
-  router.put('/mine', async (req, res, next) => {
+   router.put('/mine', async (req, res, next) => {
       try {
          const { id } = req.user;
-      
          const response = await CartServiceInstance.get({ id });
+
          res.status(200).send(response);
       } catch(err) {
          next(err);
       }
-  });
+   });
 
 
-  router.post('/mine', async (req, res, next) => {
+   router.post('/mine', async (req, res, next) => {
       try {
          const { id } = req.user;
-      
          const response = await CartServiceInstance.create({ userId: id });
 
          res.status(200).send(response);
       } catch(err) {
          next(err);
       }
-  });
+   });
 
 
-  router.post('/mine/items', async (req, res, next) => {
+   router.post('/mine/items', async (req, res, next) => {
       try {
          const { id } = req.user;
          const data = req.body;
-      
          const response = await CartServiceInstance.addItem(id, data);
 
          res.status(200).send(response);
       } catch(err) {
          next(err);
       }
-  });
+   });
 
 
-  router.put('/mine/items/:cartItemId', async (req, res, next) => {
+   router.put('/mine/items/:cartItemId', async (req, res, next) => {
       try {
          const { cartItemId } = req.params;
          const data = req.body;
-      
          const response = await CartServiceInstance.updateItem(cartItemId, data);
 
          res.status(200).send(response);
       } catch(err) {
          next(err);
       }
-  });
+   });
 
 
-  router.delete('/mine/items/:cartItemId', async (req, res, next) => {
+   router.delete('/mine/items/:cartItemId', async (req, res, next) => {
       try {
          const { cartItemId } = req.params;
-      
          const response = await CartServiceInstance.removeItem(cartItemId);
 
          res.status(200).send(response);
       } catch(err) {
          next(err);
       }
-  });
+   });
 
 
-  router.post('/mine/checkout', async (req, res, next) => {
-    try {
-      const { id } = req.user;
+   // Set up checkout endpoint
+   // Add the logic for handling checkout.
+   router.post('/mine/checkout', async (req, res, next) => {
+      try {
+         const { id } = req.user;
+         const { cartId, paymentInfo } = req.body; 
+         const response = await CartServiceInstance.checkout(cartId, id, paymentInfo);
 
-      const { cartId, paymentInfo } = req.body; 
-
-      const response = await CartServiceInstance.checkout(cartId, id, paymentInfo);
-
-      res.status(200).send(response);
-    } catch(err) {
-      next(err);
-    }
-  });
+         res.status(200).send(response);
+      } catch(err) {
+         next(err);
+      }
+   });
 }
