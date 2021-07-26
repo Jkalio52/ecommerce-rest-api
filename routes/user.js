b86a58fbd8e6ec06863fd241bb37506174e6b2e1
Jay Kalio <jerrykalio@gmail.com>
2021-07-25 19:05:45 -0500

    Set up user endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 // API endpoints that will be used in retrieving data from your database
+// Set up user endpoints
 const express = require('express');
 const router = express.Router();
 const UserService = require('../services/UserService');
@@ -6,14 +7,14 @@ const UserService = require('../services/UserService');
 const UserServiceInstance = new UserService();
 
 
+// Add the logic for handling CRUD operations related to users and their accounts
 module.exports = (app) => {
+   app.use('/users', router);
 
-  app.use('/users', router);
-
-  router.get('/:userId', async (req, res, next) => {
-     try {
-        const { userId } = req.params;
-        const response = await UserServiceInstance.get({ id: userId });
+   router.get('/:userId', async (req, res, next) => {
+      try {
+         const { userId } = req.params;
+         const response = await UserServiceInstance.get({ id: userId });
 
          res.status(200).send(response);
       } catch(err) {
@@ -22,11 +23,11 @@ module.exports = (app) => {
    });
 
 
-  router.put('/:userId', async (req, res, next) => {
-     try {
-        const { userId } = req.params;
-        const data = req.body;
-        const response = await UserServiceInstance.update({ id: userId, ...data });
+   router.put('/:userId', async (req, res, next) => {
+      try {
+         const { userId } = req.params;
+         const data = req.body;
+         const response = await UserServiceInstance.update({ id: userId, ...data });
 
          res.status(200).send(response);
       } catch(err) {
